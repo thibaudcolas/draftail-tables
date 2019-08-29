e69c8ce5a0503c4efa8736e3c455447c05934b62
Thibaud <thibaudcolas@gmail.com>
2019-08-29 02:22:27 +0000

    Deploy thibaudcolas/draftail-tables to github.com/thibaudcolas/draftail-tables.git:gh-pages

---
+++ precache-manifest.4ab71344d42c54cb1543633a6179e2fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06a7926b3dd4e6de3614b7a56388f04c",
+    "url": "/draftail-tables/index.html"
+  },
+  {
+    "revision": "5a2ecca02b930e21f647",
+    "url": "/draftail-tables/static/css/2.482d9d98.chunk.css"
+  },
+  {
+    "revision": "135e188893c4c3763b2e",
+    "url": "/draftail-tables/static/css/main.1fb10a73.chunk.css"
+  },
+  {
+    "revision": "5a2ecca02b930e21f647",
+    "url": "/draftail-tables/static/js/2.ad40c2af.chunk.js"
+  },
+  {
+    "revision": "135e188893c4c3763b2e",
+    "url": "/draftail-tables/static/js/main.61dcead5.chunk.js"
+  },
+  {
+    "revision": "5b992c76bb1426d93ed5",
+    "url": "/draftail-tables/static/js/runtime~main.ba4864dc.js"
+  }
+]);
